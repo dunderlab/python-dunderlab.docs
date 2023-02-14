bb1f8904cf6a8d62e0c38c30d355c9292e848587
Yeison Cardona <yencardonaal@unal.edu.co>
2023-02-14 11:23:53 -0500

    Release 0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dunderlab-docs',
-    version='0.6',
+    version='0.7',
     packages=['dunderlab.docs'],
     author='Yeison Cardona',
     author_email='yencardonaal@unal.edu.co',
